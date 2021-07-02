bbb5c75e3f02b95324a20e343af37969ebb3cbd8
Amimul <1805056@ugrad.cse.buet.ac.bd>
2021-07-02 23:02:55 +0600

    submitted.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main( String[] args ) {
-        
+    public static void main( String[] args ) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("input.txt"));
+
+        int number = scanner.nextInt();
+        int target = scanner.nextInt();
+
+        int[] faces = new int[number];
+
+        for (int i = 0; i < number; i++)
+            faces[i] = scanner.nextInt();
+
+        System.out.println(Offline10.rollingDiceProblemUsingDp(number, faces, target));
+
+//        System.out.println(Offline10.rollDiceSolutionNaive(number, faces, target));
     }
 }
--- src/Offline10.java
+++ src/Offline10.java
@@ -1,24 +1,31 @@
 public class Offline10 {
 
-    public static long noOfWays( int numberOfDices, int[] faces, int targetSum ) {
+    private static int modulus( int number ) {
+        return (int) (number % (Math.pow(10, 9) + 7));
+    }
 
-        long[][] summationTable = new long[numberOfDices + 1][targetSum + 1];
+    public static int rollingDiceProblemUsingDp( int diceNumber, int[] faces, int targetSum ) {
 
-        for (int j = 1; j <= faces[0] && j <= targetSum; j++)
-            summationTable[1][j] = 1;
+        int[][] summationTable = new int[diceNumber + 1][targetSum + 1];
 
-        for (int i = 2; i <= numberOfDices; i++) {
-            for (int j = 1; j <= targetSum; j++) {
-                for (int k = 1; k < j && k <= faces[i - 1]; k++)
-                    summationTable[i][j] += summationTable[i - 1][j - k];
+        summationTable[0][0] = 1;
+
+        for (int i = 1; i <= diceNumber; i++) {
+
+            for (int j = i; j <= targetSum; j++) {
+
+                summationTable[i][j] = modulus(summationTable[i][j - 1] + summationTable[i - 1][j - 1]);
+
+                if (j > faces[i - 1])
+                    summationTable[i][j] -= summationTable[i - 1][j - faces[i - 1] - 1];
             }
         }
 
-        return summationTable[numberOfDices][targetSum];
+        return modulus(summationTable[diceNumber][targetSum]);
     }
 
 
-    public static int rollDice( int numberOfDices, int[] faces, int targetSum ) {
+    public static int rollDiceSolutionNaive( int numberOfDices, int[] faces, int targetSum ) {
 
         if (numberOfDices == 0 && targetSum == 0)
             return 1;
@@ -32,9 +39,9 @@ public class Offline10 {
         int result = 0;
 
         for (int i = 1; i <= faces[numberOfDices - 1]; i++) {
-            result += rollDice(numberOfDices - 1, faces, targetSum - i);
+            result += rollDiceSolutionNaive(numberOfDices - 1, faces, targetSum - i);
         }
 
-        return result;
+        return (int) (result % (Math.pow(10, 9) + 7));
     }
 }
